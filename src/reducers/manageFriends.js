267c0c25ac410dd184f4bdf423136491a718fa36
hdp2112 <hdp2112@columbia.edu>
2020-01-23 13:53:24 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            let friend = action.friend;
+            return {...state, friends: [...state.friends, friend]}
+            break;
+        case "REMOVE_FRIEND":
+            let filter = state.friends.filter(friend => friend.id !== action.id)
+            return {...state, friends: filter}
+            
+            break;
+        default:
+            return state;
+            break;
+    }
 }
